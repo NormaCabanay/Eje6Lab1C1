15ed71f6d736604df2699993a80667a4f0eaa4b8
NormaCabanay <nbcabanay@gmail.com>
2023-09-04 19:44:38 -0300

    Initial commit

---
+++ src/eje6lab1c1/modelo/Categoria.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eje6lab1c1.modelo;
+
+/**
+ *
+ * @author norma
+ */
+public enum Categoria {
+   COMESTIBLE,
+   LIMPIEZA,
+   PERFUMERIA
+}
